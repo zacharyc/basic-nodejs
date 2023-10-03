e7770539cc75d656123531f0950cc896bfbab1ab
Zachary Cohen <zachary@zacharyc.com>
2023-10-03 15:23:43 -0400

    feat: adding in a couple of ejs pages and getting something to render

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// set the view engine to ejs
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-  res.send("Hello This World!");
+  // res.send("Hello This World, dynamically!");
+  res.render("pages/index");
+});
+
+app.get("/foo", (req, res) => {
+  res.send("Hello foo!");
 });
 
 app.listen(port, () => {
