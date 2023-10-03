8bc6b46f44557bb8d92aebf005a79a8e875c55ba
Zachary Cohen <zachary@zacharyc.com>
2023-10-03 14:49:33 -0400

    feat: adding in expressjs to try out a basic request

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const http = require('http');
-const PORT = 3000;
+const express = require("express");
+const app = express();
+const port = 3000;
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello This World!");
 });
 
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
 });
